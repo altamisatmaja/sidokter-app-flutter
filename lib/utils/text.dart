eb73705676b70a81de7bc1dbe604d7936127e651
altamisatmaja <altamisfattah16@gmail.com>
2024-01-05 21:17:05 +0700

    setup: create folder components, screens, utils -> config and text

---
+++ lib/utils/text.dart
@@ -0,0 +1,11 @@
+class AppText {
+  static final enText = {
+    'welcome_text': 'Welcome',
+    'signIn_text': 'Sign in to your account',
+    'registered_text': 'Sudah punya akun?',
+    'register_text': 'Ndang gae akun su',
+    'signUp_text': 'Dont\'t have an account?',
+    'social-login': 'Atau lanjutkan dengan akun sosial',
+    'forgot-password': 'Lupa akun?'
+  };
+}
