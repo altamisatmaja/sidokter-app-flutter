35e217bb0ac8daa811c2b1006e6c604c5fa758ef
altamisatmaja <altamisfattah16@gmail.com>
2024-01-06 00:19:52 +0700

    feat: login

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:sidokter_app/components/login_form.dart';
 import 'package:sidokter_app/utils/config.dart';
 import 'package:sidokter_app/utils/text.dart';
 
 class AuthPage extends StatefulWidget {
   // const AuthPage({super.key})
-  const AuthPage({Key? key}) : super(key:key);
+  const AuthPage({Key? key}) : super(key: key);
 
   @override
   State<AuthPage> createState() => _AuthPageState();
@@ -16,34 +17,49 @@ class _AuthPageState extends State<AuthPage> {
     Config().init(context);
     // login text view
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-          vertical: 15,
+        body: Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 15,
+        vertical: 15,
       ),
       child: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              AppText.enText['welcome_text']!,
-              style: const TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            AppText.enText['welcome_text']!,
+            style: const TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Config.spaceSmall,
+          Text(
+            AppText.enText['signIn_text']!,
+            style: const TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Config.spaceSmall,
+          const LoginForm(),
+          Config.spaceSmall,
+          Center(
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                AppText.enText['forgot-password']!,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
               ),
             ),
-              Config.spaceSmall,
-              Text(
-                AppText.enText['signIn_text']!,
-              style: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-              ),
-              )
-          ],
+          )
+        ],
       )),
-      )
-    );
+    ));
   }
 }
