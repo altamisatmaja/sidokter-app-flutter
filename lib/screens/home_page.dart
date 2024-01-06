81caac99111c41935662c629f49b311cd5396a97
altamisatmaja <altamisfattah16@gmail.com>
2024-01-06 12:29:31 +0700

    feat: setup homepage & appointment

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomePge extends StatefulWidget {
+  const HomePge({super.key});
+
+  @override
+  State<HomePge> createState() => _HomePgeState();
+}
+
+class _HomePgeState extends State<HomePge> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center();
+  }
+}
